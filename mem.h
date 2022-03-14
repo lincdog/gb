338f6111c85db15697ebfd62f876721fc13c4b74
Lincoln Ombelets <lombelets@caltech.edu>
2022-03-14 12:09:52 -0400

    remove check access functions

--- mem.h
+++ mem.h
@@ -4,24 +4,6 @@
 #include "base.h"
 #include <time.h>
 
-/* Memory mapping */
-#define REGION_CHECK(x, lb, ub) (((WORD)(x)>=lb)&&((WORD)(x)<=ub))
-
-#define is_fixed_rom(x) REGION_CHECK(x, 0x0000, 0x3FFF)
-#define is_rom_selector(x) REGION_CHECK(x, 0x2000, 0x3FFF)
-#define is_banked_rom(x) REGION_CHECK(x, 0x4000, 0x7FFF)
-#define is_rom(x) REGION_CHECK(x, 0x0000, 0x7FFF)
-#define is_ram_selector(x) REGION_CHECK(x, 0x4000, 0x5FFF)
-#define is_vram(x) REGION_CHECK(x, 0x8000, 0x9FFF)
-#define is_banked_ram(x) REGION_CHECK(x, 0xA000, 0xBFFF)
-#define is_wram(x) REGION_CHECK(x, 0xC000, 0xDFFF)
-#define is_eram(x) REGION_CHECK(x, 0xE000, 0xFDFF)
-#define is_oam(x) REGION_CHECK(x, 0xFE00, 0xFE9F)
-#define is_ioreg(x) REGION_CHECK(x, 0xFF00, 0xFF7F)
-#define is_unusable(x) (is_eram(x) || REGION_CHECK(x, 0xFEA0, 0xFEFF))
-#define is_stack(x) REGION_CHECK(x, 0xFF80, 0xFFFE)
-#define is_ram(x) (REGION_CHECK(x, 0x8000, 0xDFFF) || is_stack(x) || is_ioreg(x))
-
 #define UNINIT 0xFF
 
 /* Function prototypes */
@@ -39,9 +21,6 @@ BYTE *get_mem_pointer(GBState *, WORD, BYTE);
 #define __GET_PTR_ARGS_DECL GBState *state, WORD rel_addr, BYTE flags
 #define __GET_PTR_ARGS state, rel_addr, flags
 #define GET_PTR_FUNC(__name) static inline BYTE *__name(__GET_PTR_ARGS_DECL)
-#define __CHECK_ACCESS_ARGS_DECL GBState *state, WORD rel_addr, BYTE flags
-#define __CHECK_ACCESS_ARGS state, rel_addr, flags
-#define CHECK_ACCESS_FUNC(__name) static inline int __name(__CHECK_ACCESS_ARGS_DECL)
 
 /* Memory access flags
 7 | 6 | 5 | 4 | 3 | 2  1  0
@@ -72,11 +51,9 @@ access functions. */
 typedef struct {
     char name[6];
     WORD addr;
-    int (*check_access)(GBState *, WORD, BYTE);
     BYTE (*read)(GBState *, WORD, BYTE);
     BYTE (*write)(GBState *, WORD, BYTE, BYTE);
 } IOReg_t;
-
 
 enum cart_type {
     CART_ROM=0,
